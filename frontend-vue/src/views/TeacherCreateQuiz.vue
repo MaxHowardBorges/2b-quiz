1078b1a1761fa4a53856b22b52b5ad81c27cdf97
bm105913 <maxime.borges@etu.unice.fr>
2023-10-23 17:39:11 +0200

    Quiz creation #5

--- frontend-vue/src/views/TeacherCreateQuiz.vue
+++ frontend-vue/src/views/TeacherCreateQuiz.vue
@@ -3,44 +3,43 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
   <div class="teacher-interface">
-    <h1>Création d'un questionnaire</h1>
+    <h1>Quiz Creation</h1>
 
     <input
       id="QuizName"
       type="text"
-      placeholder="Nom du questionnaire"
+      placeholder="Quiz name"
       v-model="QuizName"
       style="border: 1px solid #000000;margin-bottom: 20px"   />
 
 
-  <div style='margin-bottom: 40px'>
+  <div class='selectDropdown' style='margin-bottom: 40px'>
     <select id="typeInput" name="typeInput" class="input">
-      <option>A choix multiples</option>
-      <option>Ouverte</option>
-      <option>Vrai/Faux</option>
+      <option>Multiple choice question</option>
+      <option>Open question</option>
+      <option>True/False question</option>
     </select></div>
 <div id='qna'>
-    <input id='Question' type='text' placeholder='Entrez votre question' style="border: 1px solid #000000;margin-bottom: 20px;height:50px;"/>
+    <input id='Question' type='text' placeholder='Type you question' style="border: 1px solid #000000;margin-bottom: 20px;height:50px;"/>
 
   <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
 remove
-</span></button><input id='Reponse' type='text' placeholder='Choix 1' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+</span></button><input id='Reponse' type='text' placeholder='Choice 1' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
 radio_button_unchecked
 </span></button></div>
   <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
 remove
-</span></button><input id='Reponse' type='text' placeholder='Choix 2' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+</span></button><input id='Reponse' type='text' placeholder='Choice 2' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
 radio_button_unchecked
 </span></button></div>
   <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
 remove
-</span></button><input id='Reponse' type='text' placeholder='Choix 3' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-radio_button_unchecked
+</span></button><input id='Reponse' type='text' placeholder='Choice 3' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span id='check' onclick='"radio_button_checked"' class="material-symbols-outlined">
 </span></button></div>
 
 </div>
 
-    <div><button class="validate" type="button">Valider quiz</button></div>
+    <div><button class="validate" type="button">Create quiz</button></div>
   </div>
 
 
@@ -57,6 +56,15 @@ radio_button_unchecked
 
 
 <style scoped>
+
+  #check:before {
+    content: "radio_button_unchecked";
+  }
+
+  #check {
+
+  }
+
   .teacher-interface {
     text-align: center;
     margin: 0 auto;
@@ -86,7 +94,6 @@ radio_button_unchecked
     display: grid;
   }
 
-
   .validate {
     background-color: #f1dc66;
     border: none;
@@ -115,12 +122,43 @@ radio_button_unchecked
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   }
 
-   .material-symbols-outlined{
+  .material-symbols-outlined{
      font-variation-settings:
        'FILL' 0,
        'wght' 400,
        'GRAD' 0,
        'opsz' 24
-   }
+  }
+
+  #QuizName{
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #5e6567;
+    box-sizing: border-box;
+  }
+
+
+  #QuizName::placeholder {
+    color: #5e6567;
+    opacity: 50%;
+    margin-left: 50%;
+  }
+
+
+  .selectDropdown {
+    box-sizing: border-box;
+    align-items: center;
+    display: inline-block;
+    box-sizing: border-box;
+
+  }
+  .selectDropdown select {
+    border: 3px solid lightgrey;
+  }
+  .selectDropdown select:active, .selectDropdown select:focus {
+    outline: none;
+  }
 
 </style>
