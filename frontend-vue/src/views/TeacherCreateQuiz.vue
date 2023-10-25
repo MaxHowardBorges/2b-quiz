3df15ee4aa97a3be18242802ec05b6c4d212b49f
bm105913 <maxime.borges@etu.unice.fr>
2023-10-25 17:31:02 +0200

    Quiz creation different question types #5

--- frontend-vue/src/views/TeacherCreateQuiz.vue
+++ frontend-vue/src/views/TeacherCreateQuiz.vue
@@ -1,71 +1,11 @@
-<template>
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-  <div class="teacher-interface">
-    <h1>Quiz Creation</h1>
-
-    <input
-      id="QuizName"
-      type="text"
-      placeholder="Quiz name"
-      v-model="QuizName"
-      style="border: 1px solid #000000;margin-bottom: 20px"   />
+<script setup>
 
 
-  <div class='selectDropdown' style='margin-bottom: 20px;margin-top: 40px'>
-    <select id="typeInput" name="typeInput" class="input" onchange='displayq(this.value)'>
-      <option value='1'>Multiple choice question</option>
-      <option value='2'>Open question</option>
-      <option value='3'>True/False question</option>
-    </select></div>
+  import {ref} from "vue";
 
-<div id='qna'>
-
-    <input id='Question' type='text' placeholder='Type you question' style="border: 1px solid #000000;margin-bottom: 20px;height:50px;"/>
-
-<div class='multiple' style="display:contents">
-  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-remove
-</span></button><input id='Reponse' type='text' placeholder='Choice 1' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-radio_button_unchecked
-</span></button></div>
-  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-remove
-</span></button><input id='Reponse' type='text' placeholder='Choice 2' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-radio_button_unchecked
-</span></button></div>
-  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
-remove
-</span></button><input id='Reponse' type='text' placeholder='Choice 3' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span id='check' class="material-symbols-outlined">
-radio_button_unchecked
-</span></button></div>
-</div>
-
-<div class='truefalse' style="display:contents" >
-
-  <div style='display:flex;margin-bottom: 20px;margin-top: 20px'><button onclick="var buttons = document.getElementsByClassName('validate2');
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
-        }this.style['background']='#f1dc66';" class="validate2" type="button">True</button><button onclick="var buttons = document.getElementsByClassName('validate2');
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
-        }this.style['background']='#f1dc66';" class="validate2" type="button">False</button></div>
-
-</div>
-
-</div>
-
-    <div><button class="validate" type="button">Create quiz</button></div>
-  </div>
-
-
-
-
-
-</template>
-
-<script>
+    const showMCQ = ref(false);
+    const showtf = ref(false);
 
   function changeColor(x) {
     var buttons = document.getElementsByClassName('validate2');
@@ -96,6 +36,82 @@ radio_button_unchecked
   }
 
 </script>
+
+<template>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+  <div class="teacher-interface">
+    <h1>Quiz Creation</h1>
+
+    <input
+      id="QuizName"
+      type="text"
+      placeholder="Quiz name"
+      v-model="QuizName"
+      style="border: 1px solid #000000;margin-bottom: 20px"   />
+
+    <div>
+      <button @click='showMCQ=true;showtf=false' onclick="var buttons = document.getElementsByClassName('qtype');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
+        }this.style['background']='#f1dc66';" class="qtype" type="button">Multiple Choice question</button><button @click="showtf = false;showMCQ=false" onclick="var buttons = document.getElementsByClassName('qtype');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
+        }this.style['background']='#f1dc66';" class="qtype" type="button">Open question</button>
+      <button @click="showtf = true;showMCQ=false"
+              onclick="var buttons = document.getElementsByClassName('qtype');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
+        }this.style['background']='#f1dc66';" style='margin-bottom: 20px' class="qtype" type="button">True/False question</button>
+    </div>
+
+<div id='qna'>
+
+    <input id='Question' type='text' placeholder='Type you question' style="border: 1px solid #000000;margin-bottom: 20px;height:50px;"/>
+
+<div class='multiple' v-show="showMCQ" >
+  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+remove
+</span></button><input id='Reponse' type='text' placeholder='Choice 1' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+radio_button_unchecked
+</span></button></div>
+  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+remove
+</span></button><input id='Reponse' type='text' placeholder='Choice 2' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+radio_button_unchecked
+</span></button></div>
+  <div id='icons'><button style='border: 0;background-color: transparent;cursor:pointer;'><span class="material-symbols-outlined">
+remove
+</span></button><input id='Reponse' type='text' placeholder='Choice 3' style="border: 1px solid #000000;margin-bottom: 10px"/><button style='border: 0;background-color: transparent;cursor:pointer;'><span id='check' class="material-symbols-outlined">
+radio_button_unchecked
+</span></button></div>
+</div>
+
+<div class='truefalse' v-show="showtf">
+
+  <div style='display:flex;margin-bottom: 20px;margin-top: 20px'><button onclick="var buttons = document.getElementsByClassName('validate2');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
+        }this.style['background']='#f1dc66';" class="validate2" type="button">True</button><button onclick="var buttons = document.getElementsByClassName('validate2');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = '#605E73'; // Réinitialise la couleur de fond de tous les boutons
+        }this.style['background']='#f1dc66';" class="validate2" type="button">False</button></div>
+
+</div>
+
+</div>
+
+    <div><button class="validate" type="button">Create quiz</button></div>
+  </div>
+
+
+
+
+
+</template>
+
+
 
 
 
@@ -188,6 +204,34 @@ radio_button_unchecked
   }
 
   .validate2:active {
+    transform: scale(1);
+    background-position: 500px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+
+  .qtype {
+    background-color: #605E73;
+    border: none;
+    color: white;
+    padding: 14px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 8px;
+    margin: 8px 0;
+    font-size: 15px;
+    cursor: pointer;
+    width: 100%;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+
+  .qtype:hover {
+    transform: scale(1.06);
+    background-position: -60px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+
+  .qtype:active {
     transform: scale(1);
     background-position: 500px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
