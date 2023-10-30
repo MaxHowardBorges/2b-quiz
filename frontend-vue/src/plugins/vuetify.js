1643174119a0044fbfa5bed8fdf440f397324eb6
Tamas Palotas <tamas.palotas@etu.univ-cotedazur.fr>
2023-10-30 21:50:41 +0100

    Define theme #17

--- frontend-vue/src/plugins/vuetify.js
+++ frontend-vue/src/plugins/vuetify.js
@@ -1,6 +1,37 @@
+import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import { createVuetify } from 'vuetify';
+import { md, aliases } from 'vuetify/iconsets/md';
+
+let defaultColors = {
+  primary: '#F6C70A',
+};
+
+const lightTheme = {
+  dark: false,
+  colors: Object.assign({}, {}, defaultColors),
+};
+
+const darkTheme = { dark: true, colors: Object.assign({}, {}, defaultColors) };
+
 export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'lightTheme',
+    variations: {
+      colors: ['primary', 'secondary'],
+      lighten: 1,
+      darken: 2,
+    },
+    themes: {
+      lightTheme,
+      darkTheme,
+    },
   },
 });
