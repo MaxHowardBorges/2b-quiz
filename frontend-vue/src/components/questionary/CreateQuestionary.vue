9085b0cef40c66d18ea499baf200b3199a1c927e
bm105913 <maxime.borges@etu.unice.fr>
2023-11-07 11:55:45 +0100

    Quiz creation different question types (w/ga101989) #5

--- frontend-vue/src/components/questionary/CreateQuestionary.vue
+++ frontend-vue/src/components/questionary/CreateQuestionary.vue
@@ -4,13 +4,53 @@
 
     <!-- Sélection du type de question -->
     <div class="question-type">
-      <button @click="selectQuestionType('vrai-ou-faux')">type de question</button>
-      <button @click="selectQuestionType('nuage-de-mots')">type de question</button>
-      <button @click="selectQuestionType('reponse-multiple')">type de question</button>
+      <button @click="selectQuestionType('vrai-ou-faux')">Vrai ou Faux</button>
+      <button @click="selectQuestionType('nuage-de-mots')">Nuage de mots</button>
+      <button @click="selectQuestionType('reponse-multiple')">Question à choix multiple</button>
     </div>
 
+
+
     <!-- Nom du questionnaire -->
-    <div class="questionnaire-name">
+    <div v-if='selectedQuestionType==="reponse-multiple" || this.selectedQuestionType===null'>
+    <div >
+      <label for="questionnaire-name">Nom du Questionnaire : </label>
+      <input type="text" id="questionnaire-name" v-model="questionnaireName">
+    </div>
+    <!-- Création de question -->
+    <div class="create-question">
+      <label for="question">Question : </label>
+      <input type="text" id="question" v-model="question">
+    </div>
+    <!-- Réponses possibles -->
+    <div class="answers">
+      <div v-for="(index) in answers" :key="index">
+        <label :for="'answer-' + index">Réponse {{ index + 1 }} : </label>
+        <input type="text" :id="'answer-' + index" v-model="answers[index].text">
+        <input type="radio" :id="'correct-answer-' + index" v-model="correctAnswer" :value="index">
+        <label :for="'correct-answer-' + index">Correcte</label>
+      </div>
+    </div>
+  </div>
+
+
+    <div v-if='this.selectedQuestionType==="nuage-de-mots"'>
+    <div>
+      <label for="questionnaire-name">Nom du Questionnaire : </label>
+      <input type="text" id="questionnaire-name" v-model="questionnaireName">
+    </div>
+
+    <!-- Création de question -->
+    <div class="create-question">
+      <label for="question">Question : </label>
+      <input type="text" id="question" v-model="question">
+    </div>
+
+    </div>
+
+
+    <div v-if='this.selectedQuestionType==="vrai-ou-faux"'>
+    <div>
       <label for="questionnaire-name">Nom du Questionnaire : </label>
       <input type="text" id="questionnaire-name" v-model="questionnaireName">
     </div>
@@ -23,14 +63,18 @@
 
     <!-- Réponses possibles -->
     <div class="answers">
-      <div v-for="(answer, index) in answers" :key="index">
-        <label :for="'answer-' + index">Réponse {{ index + 1 }} : </label>
-        <input type="text" :id="'answer-' + index" v-model="answers[index].text">
-        <input type="radio" :id="'correct-answer-' + index" v-model="correctAnswer" :value="index">
-        <label :for="'correct-answer-' + index">Correcte</label>
+      <div>
+        <input type="radio" :id="true" v-model="correctAnswer" :value="true">
+        <label>Vrai</label>
+        <input type="radio" :id="false" v-model="correctAnswer" :value="false">
+        <label>Faux</label>
 
       </div>
     </div>
+    </div>
+
+
+
 
     <!-- Ajouter une réponse -->
     <button @click="addAnswer">Ajouter une Réponse</button>
@@ -42,7 +86,7 @@
   </div>
 </template>
 
-<script>
+<script >
   export default {
     data() {
       return {
@@ -78,6 +122,7 @@
   .create-questionnaire-page {
     text-align: center;
     margin-top: 20px;
+    background-color: white;
   }
 
   h1 {
--- frontend-vue/src/views/QuestionaryView.vue
+++ frontend-vue/src/views/QuestionaryView.vue
@@ -6,7 +6,7 @@
 
 <script>
   // @ is an alias to /src
-  import CreateQuestionary from '@/components/CreateQuestionary.vue'
+  import CreateQuestionary from '@/components/questionary/CreateQuestionary.vue'
 
   export default {
     name: 'CreateQuestionaryView',
