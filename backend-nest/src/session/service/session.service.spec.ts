fad051b4e7b76feb839a3d1454a97c0cc1d5ad94
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-14 15:22:44 +0100

    Unit Tests Session service (create session+ mocks)  #16

--- backend-nest/src/session/service/session.service.spec.ts
+++ backend-nest/src/session/service/session.service.spec.ts
@@ -25,6 +25,7 @@ describe('SessionService', () => {
 
   const mockMap = {
     set: jest.fn(),
+    get: jest.fn(),
   };
 
   const mockAnswerMapper = {};
@@ -90,4 +91,127 @@ describe('SessionService', () => {
       expect(testSession).not.toEqual(session);
     });
   });
+
+  describe('Create Session', () => {
+    it('createSession : Should be return a session', async () => {
+      let question: Question[] = null;
+
+      /*{
+        questions: [
+          {
+            id: 36,
+            content: 'Quelle est la capitale du Maroc?',
+            answers: [
+              {
+                id: 104,
+                content: 'Tunis',
+                isCorrect: false,
+              },
+              {
+                id: 105,
+                content: 'Aggrabah',
+                isCorrect: false,
+              },
+              {
+                id: 106,
+                content: 'Rabat',
+                isCorrect: true,
+              },
+            ],
+          },
+          {
+            id: 37,
+            content: 'Qui a écrit "Romeo et Juliette"?',
+            answers: [
+              {
+                id: 107,
+                content: 'William Shakespeare',
+                isCorrect: true,
+              },
+              {
+                id: 108,
+                content: 'Charles Dickens',
+                isCorrect: false,
+              },
+              {
+                id: 109,
+                content: 'Jane Austen',
+                isCorrect: false,
+              },
+              {
+                id: 110,
+                content: 'George Orwell',
+                isCorrect: false,
+              },
+            ],
+          },
+          {
+            id: 38,
+            content: "Quel est le symbole chimique de l'oxygène?",
+            answers: [
+              {
+                id: 111,
+                content: 'O',
+                isCorrect: true,
+              },
+              {
+                id: 112,
+                content: 'H',
+                isCorrect: false,
+              },
+              {
+                id: 113,
+                content: 'C',
+                isCorrect: false,
+              },
+              {
+                id: 114,
+                content: 'N',
+                isCorrect: false,
+              },
+              {
+                id: 115,
+                content: 'S',
+                isCorrect: false,
+              },
+            ],
+          },
+        ],
+      };*/
+
+      mockQuestionService.findAllWithQuestion.mockResolvedValue(null);
+      const sessionconst = await service.createSession('111111');
+      expect(sessionconst instanceof Session).toBe(true);
+      expect(typeof sessionconst.id).toBe('string');
+      expect(sessionconst.id).toEqual('111111');
+    });
+  });
+
+  /*describe('nextQuestion', () => {
+    it('nextQuestion : should be returned a question', async () => {
+      let session: Session = {
+        id: '111111',
+
+        questionList: null,
+
+        //[{     id: 1,     content: "string",     answers: [{       id: 1,       content: "string",       isCorrect: true,     }],   }],
+
+        questionNumber: -1,
+
+        connectedUser: null,
+
+        userAnswers: null,
+
+        endSession: false,
+      };
+      mockQuestionService.findAllWithQuestion.mockResolvedValue(null);
+      mockMap.get.mockResolvedValue(session);
+      const sessionconst = await service.createSession('111111');
+      sessionconst.questionNumber = -1;
+      //sessionconst.questionList.length = 1;
+      let test = await service.nextQuestion('111111');
+      //expect(typeof test).toBe('string');
+      expect(test instanceof Question).toBe(true);
+    });
+  });*/
 });
