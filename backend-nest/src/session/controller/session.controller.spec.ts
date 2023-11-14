197195440cbaeb7a67804a42aedf38ca4f33bb1b
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-14 15:32:07 +0100

    Clean code session package

--- backend-nest/src/session/controller/session.controller.spec.ts
+++ backend-nest/src/session/controller/session.controller.spec.ts
@@ -2,13 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SessionController } from './session.controller';
 import { SessionService } from '../service/session.service';
 import { SessionMapper } from '../mapper/session.mapper';
-import { BodyEmptyException } from '../exception/bodyEmpty.exception';
-import { Question } from '../../question/entity/question.entity';
-import { Session } from '../session';
 import { QuestionService } from '../../question/service/question.service';
 import { QuestionModule } from '../../question/question.module';
 import { EventService } from '../../event/service/event.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('SessionController', () => {
   let sessionController: SessionController;
@@ -18,49 +14,10 @@ describe('SessionController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SessionController],
       providers: [SessionService, SessionMapper, QuestionService, EventService],
-      imports: [
-        QuestionModule,
-        TypeOrmModule.forRoot({
-          // Configurez TypeOrmModule pour vos tests
-          type: 'sqlite', // Par exemple, utilisez SQLite pour les tests
-          database: ':memory:', // Utilisez une base de données en mémoire pour les tests
-          entities: [Question], // Ajoutez vos entités ici
-          synchronize: true, // Synchronisez la base de données (pour les tests seulement)
-        }),
-      ],
+      imports: [QuestionModule],
     }).compile();
 
     sessionController = module.get<SessionController>(SessionController);
     sessionService = module.get<SessionService>(SessionService);
   });
-
-  describe('createSession', () => {
-    it('should create a new session', async () => {
-      const result = new Session('', []); // Replace with your expected session object
-      jest.spyOn(sessionService, 'initializeSession').mockResolvedValue(result);
-
-      expect(await sessionController.createSession()).toBe(result);
-    });
-  });
-
-  describe('nextQuestion', () => {
-    it('should return the next question', () => {
-      const requestBody = { id: 'session_id' };
-      const result = {} as Question; // Replace with your expected question object
-
-      jest.spyOn(sessionService, 'nextQuestion').mockReturnValue(result);
-
-      expect(sessionController.nextQuestion(requestBody)).toBe(result);
-    });
-
-    it('should throw BodyEmptyException when id is undefined', () => {
-      const requestBody = { id: undefined };
-
-      expect(() => sessionController.nextQuestion(requestBody)).toThrow(
-        BodyEmptyException,
-      );
-    });
-  });
-
-  // Add similar test cases for other methods like joinSession, getCurrentQuestion, respondQuestion, and getMap
 });
--- backend-nest/src/session/service/session.service.spec.ts
+++ backend-nest/src/session/service/session.service.spec.ts
@@ -5,7 +5,6 @@ import { AnswerMapper } from '../../question/mapper/answer.mapper';
 import { EventService } from '../../event/service/event.service';
 import { Session } from '../session';
 import { Question } from '../../question/entity/question.entity';
-import { Answer } from '../../question/entity/answer.entity';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -70,7 +69,6 @@ describe('SessionService', () => {
       expect(typeof test).toBe('string');
     });
     it('initializeSession : should be not equal to the empty session', async () => {
-      //let sessionMap: Map<string, Session> = new Map<string, Session>();
       let session = {
         id: '111111',
 
@@ -84,9 +82,6 @@ describe('SessionService', () => {
 
         endSession: false,
       };
-      //sessionMap.set('111111', session);
-      //mockMap.set.mockResolvedValue("111111", session);
-      //mockMap.set.mockResolvedValue(null);
       let testSession = await service.initializeSession();
       expect(testSession).not.toEqual(session);
     });
@@ -95,90 +90,6 @@ describe('SessionService', () => {
   describe('Create Session', () => {
     it('createSession : Should be return a session', async () => {
       let question: Question[] = null;
-
-      /*{
-        questions: [
-          {
-            id: 36,
-            content: 'Quelle est la capitale du Maroc?',
-            answers: [
-              {
-                id: 104,
-                content: 'Tunis',
-                isCorrect: false,
-              },
-              {
-                id: 105,
-                content: 'Aggrabah',
-                isCorrect: false,
-              },
-              {
-                id: 106,
-                content: 'Rabat',
-                isCorrect: true,
-              },
-            ],
-          },
-          {
-            id: 37,
-            content: 'Qui a écrit "Romeo et Juliette"?',
-            answers: [
-              {
-                id: 107,
-                content: 'William Shakespeare',
-                isCorrect: true,
-              },
-              {
-                id: 108,
-                content: 'Charles Dickens',
-                isCorrect: false,
-              },
-              {
-                id: 109,
-                content: 'Jane Austen',
-                isCorrect: false,
-              },
-              {
-                id: 110,
-                content: 'George Orwell',
-                isCorrect: false,
-              },
-            ],
-          },
-          {
-            id: 38,
-            content: "Quel est le symbole chimique de l'oxygène?",
-            answers: [
-              {
-                id: 111,
-                content: 'O',
-                isCorrect: true,
-              },
-              {
-                id: 112,
-                content: 'H',
-                isCorrect: false,
-              },
-              {
-                id: 113,
-                content: 'C',
-                isCorrect: false,
-              },
-              {
-                id: 114,
-                content: 'N',
-                isCorrect: false,
-              },
-              {
-                id: 115,
-                content: 'S',
-                isCorrect: false,
-              },
-            ],
-          },
-        ],
-      };*/
-
       mockQuestionService.findAllWithQuestion.mockResolvedValue(null);
       const sessionconst = await service.createSession('111111');
       expect(sessionconst instanceof Session).toBe(true);
@@ -186,32 +97,4 @@ describe('SessionService', () => {
       expect(sessionconst.id).toEqual('111111');
     });
   });
-
-  /*describe('nextQuestion', () => {
-    it('nextQuestion : should be returned a question', async () => {
-      let session: Session = {
-        id: '111111',
-
-        questionList: null,
-
-        //[{     id: 1,     content: "string",     answers: [{       id: 1,       content: "string",       isCorrect: true,     }],   }],
-
-        questionNumber: -1,
-
-        connectedUser: null,
-
-        userAnswers: null,
-
-        endSession: false,
-      };
-      mockQuestionService.findAllWithQuestion.mockResolvedValue(null);
-      mockMap.get.mockResolvedValue(session);
-      const sessionconst = await service.createSession('111111');
-      sessionconst.questionNumber = -1;
-      //sessionconst.questionList.length = 1;
-      let test = await service.nextQuestion('111111');
-      //expect(typeof test).toBe('string');
-      expect(test instanceof Question).toBe(true);
-    });
-  });*/
 });
