05c157829fe6b76a1dbae1a0018cfc661e29d0d7
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-24 14:40:10 +0100

    Cinfiguration de jest pour les tests end to end + début de test sur sessionController #16

--- backend-nest/src/session/controller/session.controller.e2e-spec.ts
+++ backend-nest/src/session/controller/session.controller.e2e-spec.ts
@@ -2,34 +2,38 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import * as request from 'supertest';
-import { SessionController } from './session.controller';
-import { SessionService } from '../service/session.service';
-import { QuestionService } from '../../question/service/question.service';
-import { SessionMapper } from '../mapper/session.mapper';
-import { AnswerMapper } from '../../question/mapper/answer.mapper';
-import { EventService } from '../../event/service/event.service';
-import { Session } from '../session';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from '../../config/database.config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-      /*controllers: [SessionController],
-      providers: [
-        SessionService,
-        QuestionService,
-        AnswerMapper, // Incluez l'AnswerMapper ici
-        EventService,
-      ],*/
-    }).compile();
+    })
+      .overrideModule(ConfigModule)
+      .useModule(
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env.test'],
+        }),
+      )
+      .overrideModule(TypeOrmModule)
+      .useModule(
+        TypeOrmModule.forRootAsync({
+          useFactory: getTypeOrmConfig,
+          inject: [ConfigService],
+        }),
+      )
+      .compile();
 
-    app = module.createNestApplication();
+    app = module.createNestApplication({ cors: true });
     await app.init();
   });
 
-  it('/create', () => {
+  it('session/create', () => {
     return request(app.getHttpServer()).post('/session/create').expect(201);
   });
 });
