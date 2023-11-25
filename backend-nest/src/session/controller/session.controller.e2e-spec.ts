3bec40a717fc1ccb8ece4fe6cd46186f4b91073f
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-25 11:06:35 +0100

    patch v1 e2e test #16

--- backend-nest/src/session/controller/session.controller.e2e-spec.ts
+++ backend-nest/src/session/controller/session.controller.e2e-spec.ts
@@ -2,9 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import * as request from 'supertest';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getTypeOrmConfig } from '../../config/database.config';
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,12 +10,12 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
+    })/*
       .overrideModule(ConfigModule)
       .useModule(
         ConfigModule.forRoot({
           isGlobal: true,
-          envFilePath: ['.env.test'],
+          envFilePath: ['.env.local'],
         }),
       )
       .overrideModule(TypeOrmModule)
@@ -26,14 +24,21 @@ describe('AppController (e2e)', () => {
           useFactory: getTypeOrmConfig,
           inject: [ConfigService],
         }),
-      )
+      )*/
       .compile();
 
     app = module.createNestApplication({ cors: true });
     await app.init();
   });
 
-  it('session/create', () => {
-    return request(app.getHttpServer()).post('/session/create').expect(201);
+  it('question', async () => {
+    await request('http://localhost:3000') // Remplacez 3000 par le port de votre serveur
+      .get('/question')
+      .expect(200); // Assurez-vous que c'est le bon code d'état attendu pour cette requête
   });
+
+  /*it('question', async () => {
+    console.log(request(app.getHttpServer()).get('/question'));
+    return await request(app.getHttpServer()).get('/question').expect(201);
+  });*/
 });
