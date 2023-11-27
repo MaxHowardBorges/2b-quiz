bf0cd02202bf187a10fb37841a5b016edaeb32a8
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-27 13:10:45 +0100

    Test End to End, session controller #16

--- backend-nest/src/session/controller/session.controller.e2e-spec.ts
+++ backend-nest/src/session/controller/session.controller.e2e-spec.ts
@@ -2,7 +2,8 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import * as request from 'supertest';
-
+import { Session } from '../session';
+import { Question } from '../../question/entity/question.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -10,32 +11,41 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })/*
-      .overrideModule(ConfigModule)
-      .useModule(
-        ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: ['.env.local'],
-        }),
-      )
-      .overrideModule(TypeOrmModule)
-      .useModule(
-        TypeOrmModule.forRootAsync({
-          useFactory: getTypeOrmConfig,
-          inject: [ConfigService],
-        }),
-      )*/
-      .compile();
+    }).compile();
 
     app = module.createNestApplication({ cors: true });
     await app.init();
   });
 
-  it('question', async () => {
-    await request('http://localhost:3000') // Remplacez 3000 par le port de votre serveur
-      .get('/question')
-      .expect(200); // Assurez-vous que c'est le bon code d'état attendu pour cette requête
+  describe('Session (e2e)', () => {
+    it('createSession', async () => {
+      let session = await request('http://localhost:3000/session').post(
+        '/create',
+      );
+      expect(session.status).toBe(201);
+      expect(session.noContent).toBeFalsy();
+      //console.log(session.body.id);
+      expect(typeof session.body.id).toBe('string');
+    });
+    it('nextQuestion', async () => {
+      let session = await request('http://localhost:3000/session').post(
+        '/create',
+      );
+      let nextQ = await request('http://localhost:3000/session')
+        .post('/nextQuestion')
+        .send({ idSession: session.body.id });
+      //console.log(nextQ.body);
+      expect(nextQ.status).toBe(201);
+      expect(nextQ.noContent).toBeFalsy();
+      expect(nextQ.body.answers.length).toBeGreaterThanOrEqual(2);
+      expect(nextQ.body.id).not.toBeNull();
+      expect(typeof nextQ.body.content).toBe('string');
+    });
   });
+
+  /*it('question', async () => {
+    await request('http://localhost:3000').get('/question').expect(200);
+  });*/
 
   /*it('question', async () => {
     console.log(request(app.getHttpServer()).get('/question'));
