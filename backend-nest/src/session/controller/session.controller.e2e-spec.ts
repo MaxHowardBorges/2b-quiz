16ac61b5987058dc73aaa12a63cf77985dcabaf2
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-27 13:11:09 +0100

    Clean code

--- backend-nest/src/session/controller/session.controller.e2e-spec.ts
+++ backend-nest/src/session/controller/session.controller.e2e-spec.ts
@@ -2,8 +2,6 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import * as request from 'supertest';
-import { Session } from '../session';
-import { Question } from '../../question/entity/question.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -24,7 +22,7 @@ describe('AppController (e2e)', () => {
       );
       expect(session.status).toBe(201);
       expect(session.noContent).toBeFalsy();
-      //console.log(session.body.id);
+
       expect(typeof session.body.id).toBe('string');
     });
     it('nextQuestion', async () => {
@@ -34,7 +32,7 @@ describe('AppController (e2e)', () => {
       let nextQ = await request('http://localhost:3000/session')
         .post('/nextQuestion')
         .send({ idSession: session.body.id });
-      //console.log(nextQ.body);
+
       expect(nextQ.status).toBe(201);
       expect(nextQ.noContent).toBeFalsy();
       expect(nextQ.body.answers.length).toBeGreaterThanOrEqual(2);
@@ -42,13 +40,4 @@ describe('AppController (e2e)', () => {
       expect(typeof nextQ.body.content).toBe('string');
     });
   });
-
-  /*it('question', async () => {
-    await request('http://localhost:3000').get('/question').expect(200);
-  });*/
-
-  /*it('question', async () => {
-    console.log(request(app.getHttpServer()).get('/question'));
-    return await request(app.getHttpServer()).get('/question').expect(201);
-  });*/
 });
