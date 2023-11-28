ddffb3bc23b8e482c08df86b1fe39c320310400b
er104065 <rayan.el-armouzi@etu.unice.fr>
2023-11-28 10:30:51 +0100

    E2E tests session #16

---
+++ backend-nest/src/questionnary/controller/questionnary.controller.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../../app.module';
+import * as request from 'supertest';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication({ cors: true });
+    await app.init();
+  });
+
+  describe('Questionnary (e2e)', () => {
+    it('createSession', async () => {
+      let session = await request('http://localhost:3000/session').post(
+        '/create',
+      );
+      expect(session.status).toBe(201);
+      expect(session.noContent).toBeFalsy();
+
+      expect(typeof session.body.id).toBe('string');
+    });
+  });
+});
