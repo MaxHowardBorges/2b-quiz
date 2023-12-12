fd40fd7568aa5666400135e3f57184043517b792
ga101989 <axel.guillou@etu.unice.fr>
2023-12-12 11:30:03 +0100

    Account page for user change name #3 #4 #7

--- frontend-vue/src/components/user/AccountBlock.vue
+++ frontend-vue/src/components/user/AccountBlock.vue
@@ -1,29 +1,32 @@
 <template>
-
   <v-sheet
     max-width="750px"
     rounded="lg"
-    width="70%"
+    width="90%"
     class="mt-5 px-6 py-8 mx-auto"
-    elevation="5">
-
+    elevation="5"
+  >
     <h1>USER PAGE</h1>
 
+    <!-- Bind v-model to userStore.username and add @change event handler -->
     <v-text-field
       class="pa-4 mt-5"
       label="username"
+      v-model="this.name"
+      @change="updateUsername"
     ></v-text-field>
 
-    <v-text-field class='pa-4 ' label="name" ></v-text-field>
+    <v-text-field
+      class="pa-4 mt-5"
+      label="name"
+      v-model="this.name"
+    ></v-text-field>
 
-    <p> Status : <b>{{userStore.userRole}}</b></p>
-
+    <p> Status : <b>{{ userStore.userRole }}</b></p>
   </v-sheet>
-
 </template>
 
 <script>
-
   import { useUserStore } from '@/stores/userStore';
   import { ref } from 'vue';
 
@@ -31,19 +34,22 @@
     name: 'AccountBlock',
     setup() {
       const userStore = useUserStore();
-      const updateUsername = () => {
-        userStore.setUsername(userStore.username);
-      };
+
       return {
         userStore,
-        updateUsername,
-        username: ref(''),
-        password: ref(''),
+        name: ref('test'),
       };
     },
-  }
-
+    methods:{
+      updateUsername(){
+        console.log("start",this.userStore.username)
+        this.userStore.setUsername(this.name);
+        console.log("variable",this.name);
+        console.log("store",this.userStore.username)
+      },
+    }
+  };
 </script>
-<style scoped>
 
+<style scoped>
 </style>
