37a19b552bc2e007b156cedfc959acf1f1828518
ga101989 <axel.guillou@etu.unice.fr>
2024-01-12 15:26:01 +0100

    Add v-dialog for action #26

--- frontend-vue/src/components/group/ListOFGroup.vue
+++ frontend-vue/src/components/group/ListOFGroup.vue
@@ -20,37 +20,98 @@
         <td class="text-center">2B QUIZ</td>
         <td class="text-center">7</td>
         <td class="text-center">
-          <v-btn @click="" icon="adds"></v-btn>
-          <v-btn @click="" icon="logout"></v-btn>
-          <v-btn @click="" icon="delete"></v-btn>
+          <v-btn @click="openAdd" icon="add"></v-btn>
+          <v-btn @click="openLeave" icon="logout"></v-btn>
+          <v-btn @click="openDelete" icon="delete"></v-btn>
         </td>
       </tr>
       <tr class="ma-2">
         <td class="text-center">2B QUIZ</td>
         <td class="text-center">7</td>
         <td class="text-center">
-          <v-btn @click="" icon="add"></v-btn>
-          <v-btn @click="" icon="logout"></v-btn>
-          <v-btn @click="" icon="delete"></v-btn>
+          <v-btn @click="openAdd" icon="add"></v-btn>
+          <v-btn @click="openLeave" icon="logout"></v-btn>
+          <v-btn @click="openDelete" icon="delete"></v-btn>
         </td>
       </tr>
       <tr class="ma-2">
         <td class="text-center">2B QUIZ</td>
         <td class="text-center">7</td>
         <td class="text-center">
-          <v-btn @click="" icon="add"></v-btn>
-          <v-btn @click="" icon="logout"></v-btn>
-          <v-btn @click="" icon="delete"></v-btn>
+          <v-btn @click="openAdd " icon="add"></v-btn>
+          <v-btn @click="openLeave" icon="logout"></v-btn>
+          <v-btn @click="openDelete" icon="delete"></v-btn>
         </td>
       </tr>
       </tbody>
     </table>
+
+    <v-dialog v-model="addDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Add User
+        </v-card-title>
+        <v-card-actions>
+          <v-btn @click="closeDialogs">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="leaveDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Leave Group
+        </v-card-title>
+        <v-card-actions>
+          <v-btn @click="closeDialogs">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="deleteDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Delete Group
+        </v-card-title>
+        <v-card-text>Are you sure to delete this Group?</v-card-text>
+        <v-card-actions>
+          <v-btn @click="closeDialogs">Yes</v-btn>
+          <v-btn @click="closeDialogs">No</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <v-btn class="mt-5" color="primary">Create a group</v-btn>
   </v-sheet>
 </template>
 
 <script>
+
+  export default {
+    data() {
+      return {
+        addDialog: false,
+        leaveDialog: false,
+        deleteDialog: false,
+      };
+    },
+    methods: {
+      openAdd() {
+        this.addDialog = true;
+      },
+      openLeave() {
+        this.leaveDialog = true;
+      },
+      openDelete() {
+        this.deleteDialog = true;
+      },
+      closeDialogs() {
+        this.addDialog = false;
+        this.leaveDialog = false;
+        this.deleteDialog = false;
+      },
+    },
+  };
 
 </script>
 
