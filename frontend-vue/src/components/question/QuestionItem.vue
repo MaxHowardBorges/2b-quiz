84b3e39f81aa959e067c697f15edda97ac4e53a6
bm105913 <maxime.borges@etu.unice.fr>
2024-01-16 14:20:32 +0100

    Resultats Étudiants composant QuestionItem.vue restauré #25

--- frontend-vue/src/components/question/QuestionItem.vue
+++ frontend-vue/src/components/question/QuestionItem.vue
@@ -1,1 +1,43 @@
-QuestionItem.vue
+<template>
+  <v-sheet elevation="5" rounded="lg" class="d-flex flex-column my-2 pa-3">
+    <div class="mb-4"></div>
+    <span class="spacer"></span>
+    <span class="spacer"></span>
+    <v-btn id="ic" @click="showGlobalResults" text>
+      Voir les résultats globaux
+    </v-btn>
+    <v-btn id="ic" @click="toggleDropdown" text>
+      Voir les réponses correctes
+    </v-btn>
+    <v-btn id="ic" @click="showStudentResponses" text>
+      Voir les réponses des étudiants
+    </v-btn>
+  </v-sheet>
+</template>
+
+<script>
+  import { ref } from 'vue';
+
+  export default {
+    name: 'QuestionItem',
+    props: {
+      questionName: String,
+      questionnaryId: Number,
+    },
+    methods: {
+      toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
+      },
+      showGlobalResults() {
+        this.showGlobal = !this.showGlobal;
+      },
+      showStudentResponses() {
+        this.showStudentResponsesTable = !this.showStudentResponsesTable;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  /* Ajoutez du style au besoin */
+</style>
