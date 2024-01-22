36acd06e14dd83ab79ea985d3103a8066f9ef7b0
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-01-22 15:11:42 +0100

    Add loaders #25

--- frontend-vue/src/components/session/ActionTeacher.vue
+++ frontend-vue/src/components/session/ActionTeacher.vue
@@ -28,7 +28,11 @@
     <v-btn @click="cancelSession" class="btn" color="primary">
       Fin de la session
     </v-btn>
-    <v-btn @click="nextQuestion" class="btn" color="success">
+    <v-btn
+      @click="nextQuestion"
+      class="btn"
+      color="success"
+      :loading="loadingNextQuestion">
       Question suivante
     </v-btn>
     <v-btn @click="openSettings" class="btn" color="info">Settings</v-btn>
@@ -54,6 +58,7 @@
     data() {
       return {
         dialogVisible: false,
+        loadingNextQuestion: false,
       };
     },
     methods: {
@@ -74,6 +79,7 @@
         await router.push('/');
       },
       async nextQuestion() {
+        this.loadingNextQuestion = true;
         try {
           const response = await this.sessionStore.nextQuestion();
           await this.sessionStore.getCurrentQuestionForTeacher(response);
@@ -85,6 +91,7 @@
             'Error handling next question: ' + e.message,
           );
         }
+        this.loadingNextQuestion = false;
       },
       openSettings() {
         this.$refs.settingsDialog.openSettings();
