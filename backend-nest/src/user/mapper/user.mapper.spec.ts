320e266f26fd77a8593ba9aa396c1698b2491243
er104065 <rayan.el-armouzi@etu.unice.fr>
2024-01-25 23:22:30 +0100

    Unit tests user service / mapper#25

--- backend-nest/src/user/mapper/user.mapper.spec.ts
+++ backend-nest/src/user/mapper/user.mapper.spec.ts
@@ -89,4 +89,114 @@ describe('UserMapper', () => {
       }
     });
   });
+
+  // teacherGroupDataDtoMap
+  describe('teacherGroupDataDtoMap', () => {
+    it('should return a TeacherGroupDataDto', () => {
+      const user = userMock[0];
+      const dto = mapper.teacherGroupDataDtoMap(user);
+      expect(dto.id).toEqual(user.id);
+      expect(dto.username).toEqual(user.username);
+      expect(dto.name).toEqual(user.name);
+      expect(dto.surname).toEqual(user.surname);
+    });
+  });
+
+  // partialUserDtoMap
+  describe('partialUserDtoMap', () => {
+    it('should return a PartialUserDto', () => {
+      const user = userMock[0];
+      const dto = mapper.partialUserDtoMap(user);
+      expect(dto.id).toEqual(user.id);
+      expect(dto.username).toEqual(user.username);
+      expect(dto.name).toEqual(user.name);
+      expect(dto.surname).toEqual(user.surname);
+    });
+  });
+
+  // partialUserDtoListMap
+  describe('partialUserDtoListMap', () => {
+    it('should return a PartialUserDto list', () => {
+      const dto = mapper.partialUserDtoListMap(userMock);
+      expect(dto.length).toEqual(10);
+      for (let i = 0; i < 10; i++) {
+        expect(dto[i].id).toEqual(userMock[i].id);
+        expect(dto[i].username).toEqual(userMock[i].username);
+        expect(dto[i].name).toEqual(userMock[i].name);
+        expect(dto[i].surname).toEqual(userMock[i].surname);
+      }
+    });
+  });
+
+  // userFullDataDtoMap
+  describe('userFullDataDtoMap', () => {
+    it('should return a UserFullDataDto', () => {
+      const user = userMock[0];
+      const dto = mapper.userFullDataDtoMap(user);
+      expect(dto.id).toEqual(user.id);
+      expect(dto.username).toEqual(user.username);
+      expect(dto.name).toEqual(user.name);
+      expect(dto.surname).toEqual(user.surname);
+      expect(dto.validate).toEqual(user.validate);
+      expect(dto.userType).toEqual(user.getUserType());
+      expect(dto.askedDelete).toEqual(user.askedDelete);
+    });
+  });
+
+  // userFullDataDtoListMap
+  describe('userFullDataDtoListMap', () => {
+    it('should return a UserListDto', () => {
+      const dto = mapper.userFullDataDtoListMap(userMock, 10);
+      expect(dto.nbPage).toEqual(10);
+      expect(dto.userList.length).toEqual(10);
+      for (let i = 0; i < 10; i++) {
+        expect(dto.userList[i].id).toEqual(userMock[i].id);
+        expect(dto.userList[i].username).toEqual(userMock[i].username);
+        expect(dto.userList[i].name).toEqual(userMock[i].name);
+        expect(dto.userList[i].surname).toEqual(userMock[i].surname);
+        expect(dto.userList[i].validate).toEqual(userMock[i].validate);
+        expect(dto.userList[i].userType).toEqual(userMock[i].getUserType());
+        expect(dto.userList[i].askedDelete).toEqual(userMock[i].askedDelete);
+      }
+    });
+  });
+
+  // userSelfDataDtoMap
+  describe('userSelfDataDtoMap', () => {
+    it('should return a UserSelfDataDto', () => {
+      const user = userMock[0];
+      const dto = mapper.userSelfDataDtoMap(user);
+      expect(dto.username).toEqual(user.username);
+      expect(dto.name).toEqual(user.name);
+      expect(dto.surname).toEqual(user.surname);
+      expect(dto.userType).toEqual(user.getUserType());
+    });
+  });
+
+  // userDeleteDtoMap
+  describe('userDeleteDtoMap', () => {
+    it('should return a UserDeletedFullDataDto', () => {
+      const user = deletedUserMock[0];
+      const dto = mapper.userDeleteDtoMap(user);
+      expect(dto.id).toEqual(user.id);
+      expect(dto.username).toEqual(user.username);
+      expect(dto.userType).toEqual(user.getUserType());
+    });
+  });
+
+  // userDeleteDtoListMap
+  describe('userDeleteDtoListMap', () => {
+    it('should return a UserListDto', () => {
+      const dto = mapper.userDeleteDtoListMap(deletedUserMock, 10);
+      expect(dto.nbPage).toEqual(10);
+      expect(dto.userList.length).toEqual(10);
+      for (let i = 0; i < 10; i++) {
+        expect(dto.userList[i].id).toEqual(deletedUserMock[i].id);
+        expect(dto.userList[i].username).toEqual(deletedUserMock[i].username);
+        expect(dto.userList[i].userType).toEqual(
+          deletedUserMock[i].getUserType(),
+        );
+      }
+    });
+  });
 });
--- backend-nest/src/user/service/user.service.spec.ts
+++ backend-nest/src/user/service/user.service.spec.ts
@@ -18,6 +18,7 @@ import { NotValidatedUserException } from '../exception/notValidatedUser.excepti
 import { UserNotFoundException } from '../../auth/exception/userNotFound.exception';
 import { Group } from '../entity/group.entity';
 import { generateGroupMock } from '../../../test/mock/group.mock';
+import { GroupNotFoundException } from '../../questionnary/exception/groupNotFound.exception';
 
 describe('UserService', () => {
   let service: UserService;
@@ -521,4 +522,174 @@ describe('UserService', () => {
   });
 
   // unit test groups
+  // createGroup
+  describe('createGroup', () => {
+    it('should create a group', async () => {
+      const group = groupMock;
+      group.id = undefined;
+      group.tabUsers = [];
+      group.teacher = teacherMock;
+      groupRepository.save.mockResolvedValue(group);
+      await service.createGroup(group.groupName, group.teacher);
+      expect(groupRepository.save).toBeCalledWith(group);
+    });
+  });
+
+  // deleteGroup
+  describe('deleteGroup', () => {
+    it('should delete a group', async () => {
+      const group = groupMock;
+      group.tabUsers = [];
+      groupRepository.findOne.mockResolvedValue(group);
+      groupRepository.save.mockResolvedValue(group);
+      await service.deleteGroup(group.id);
+      expect(groupRepository.save).toBeCalledWith(group);
+      expect(group.tabUsers).toHaveLength(0);
+    });
+    it('should throw an error if group is not found', async () => {
+      const group = groupMock;
+      groupRepository.findOne.mockResolvedValue(null);
+      await expect(service.deleteGroup(group.id)).rejects.toThrow(
+        GroupNotFoundException,
+      );
+    });
+  });
+
+  // getGroup
+  describe('getGroup', () => {
+    it('should get a group', async () => {
+      const group = groupMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      const test = await service.getGroup(group.id);
+      expect(test).toEqual(group);
+      expect(test).toBeInstanceOf(Group);
+    });
+  });
+
+  // addUserToGroup
+  describe('addUserToGroup', () => {
+    it('should add a user to a group', async () => {
+      const group = groupMock;
+      const user = studentMock;
+      group.tabUsers = [];
+      groupRepository.findOne.mockResolvedValue(group);
+      groupRepository.save.mockResolvedValue(group);
+      userRepository.findOne.mockResolvedValue(user);
+      await service.addUserToGroup(group.id, user.id);
+      expect(groupRepository.save).toBeCalledWith(group);
+      expect(group.tabUsers).toHaveLength(1);
+    });
+    it('should throw an error if group is not found', async () => {
+      const group = groupMock;
+      const user = studentMock;
+      groupRepository.findOne.mockResolvedValue(null);
+      await expect(service.addUserToGroup(group.id, user.id)).rejects.toThrow(
+        GroupNotFoundException,
+      );
+    });
+  });
+
+  // removeUserFromGroup
+  describe('removeUserFromGroup', () => {
+    it('should remove a user from a group', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      group.tabUsers = [user];
+      groupRepository.findOne.mockResolvedValue(group);
+      groupRepository.save.mockResolvedValue(group);
+      userRepository.findOne.mockResolvedValue(user);
+      await service.removeUserFromGroup(group.id, user.id);
+      expect(groupRepository.save).toBeCalledWith(group);
+      expect(group.tabUsers).toHaveLength(0);
+    });
+    it('should throw an error if group is not found', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      groupRepository.findOne.mockResolvedValue(null);
+      await expect(
+        service.removeUserFromGroup(group.id, user.id),
+      ).rejects.toThrow(GroupNotFoundException);
+    });
+  });
+
+  // getGroupsFromTeacher
+  describe('getGroupsFromTeacher', () => {
+    it('should get groups from a teacher', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      userRepository.findOne.mockResolvedValue(user);
+      const test = await service.getGroupsFromTeacher(group.teacher);
+      expect(test).toEqual([]);
+      expect(test).toBeInstanceOf(Array);
+    });
+  });
+
+  // isGroupFromTeacher
+  describe('isGroupFromTeacher', () => {
+    it('should return true if group is from a teacher', async () => {
+      const group = groupMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      const test = await service.isGroupFromTeacher(group.id, group.teacher);
+      expect(groupRepository.findOne).toBeCalledWith({
+        relations: ['teacher', 'tabUsers'],
+        where: { id: group.id },
+      });
+      expect(test).toEqual(true);
+    });
+    it('should return false if group is not from a teacher', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      const test = await service.isGroupFromTeacher(group.id, user);
+      expect(groupRepository.findOne).toBeCalledWith({
+        relations: ['teacher', 'tabUsers'],
+        where: { id: group.id },
+      });
+      expect(test).toEqual(false);
+    });
+  });
+
+  // isAlreadyInGroup
+  describe('isAlreadyInGroup', () => {
+    it('should return true if user is already in a group', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      userRepository.findOne.mockResolvedValue(user);
+      await service.addUserToGroup(group.id, user.id);
+      const test = await service.isAlreadyInGroup(group.id, user.id);
+      expect(groupRepository.findOne).toBeCalledWith({
+        relations: ['teacher', 'tabUsers'],
+        where: { id: group.id },
+      });
+      expect(test).toEqual(true);
+    });
+    it('should return false if user is not already in a group', async () => {
+      const group = groupMock;
+      const user = teacherMock;
+      groupRepository.findOne.mockResolvedValue(group);
+      const test = await service.isAlreadyInGroup(group.id, user.id + 1);
+      expect(groupRepository.findOne).toBeCalledWith({
+        relations: ['teacher', 'tabUsers'],
+        where: { id: group.id },
+      });
+      expect(test).toEqual(false);
+    });
+  });
+
+  // getGroups
+  describe('getGroups', () => {
+    it('should get groups from a user', async () => {
+      const group = groupMock;
+      groupRepository.find.mockResolvedValue([group]);
+      const test = await service.getGroups(group.teacher);
+      expect(groupRepository.find).toBeCalledWith({
+        relations: ['teacher', 'tabUsers'],
+        where: { tabUsers: { id: group.teacher.id } },
+      });
+      expect(test).toEqual([group]);
+      expect(test).toBeInstanceOf(Array);
+    });
+  });
 });
